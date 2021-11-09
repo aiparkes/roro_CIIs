f93669f3e0725e0001692dd0d1571d236fe5ba66
Amy Parkes <amy@arcsilea.com>
2021-11-09 17:50:19 +0000

    speedup

--- distribution_example.py
+++ distribution_example.py
@@ -33,8 +33,8 @@ with open(a, 'r') as f:
 from layout import _max_width_
 _max_width_()
 
-data = pd.read_excel(r'2019-v106-01122020-EU MRV Publication of information with IHS Data.xlsx', engine='openpyxl', sheet_name='2019',skiprows=2)
-data = data[(data['Ship type']=='Ro-ro ship')]#(data['Ship type']=='Container/ro-ro cargo ship')]#(data['Ship type']=='Container/ro-ro cargo ship')]#|(data['Ship type']=='Ro-ro ship')]
+data = pd.read_csv('MRV_data.csv')
+#data = data[(data['Ship type']=='Ro-ro ship')]#(data['Ship type']=='Container/ro-ro cargo ship')]#(data['Ship type']=='Container/ro-ro cargo ship')]#|(data['Ship type']=='Ro-ro ship')]
 
 ##hide menu and footer
 hide_menu_style = """
@@ -56,30 +56,20 @@ if graph_type == 'AER':
     data['AER'] = data['AER']#cgDIST']
     xaxis_label = 'DWT'
     yaxis_label = 'AER'
+    lines = pd.read_csv('AER_lines.csv')
 
 elif graph_type == 'cgDist':
     data['DWT'] = data['GT']#
     data['AER'] = data['cgDIST']
     xaxis_label = 'GT'
     yaxis_label = 'cgDist'
+    lines = pd.read_csv('cgDist_lines.csv')
 
 data['LOG_AER'] = np.log(data.AER)
 data['LOG_DWT'] = np.log(data.DWT)
 
 cIIHeader= '<p style="font-weight: 100; color:#042E43; font-size: 24px; margin-botttom: 0; ">CII Rating Boundaries and Quantile Regressions for '+graph_type+' (Prototype) </p>'
 st.write(cIIHeader, unsafe_allow_html=True)
-
-models = quant_reg(data, [0.15,0.35,0.5,0.65,0.85])
-models = models.set_index('q')
-quant = lambda i, x : models.a[i] + x*models.b[i]
-y_func = lambda i : models.a[i] + x_range*models.b[i]
-
-big_y_func = lambda i : models.a[i]*(x_range**models.b[i])
-boundary_gaps = {}
-
-for name, quantile in [('a',0.15),('b',0.35),('d',0.65),('e',0.85)]:
-    gap = quant(quantile, np.median(data.LOG_DWT)) - quant(0.5, np.median(data.LOG_DWT))
-    boundary_gaps.update({name: gap})
 
 fig = go.Figure()
 
@@ -91,34 +81,34 @@ fig.add_trace(go.Scatter(x=np.exp(data.LOG_DWT),
 x_range = np.arange(min(data.LOG_DWT),max(data.LOG_DWT),step=(max(data.LOG_DWT)-(min(data.LOG_DWT)))/100)
 for letter, i in [('a',0.15),('b',0.35),('c',0.5),('d',0.65),('e',0.85)]:#[0.15,0.35,0.5,0.65,0.85]:
     if i == 0.5:
-        fig.add_trace(go.Scatter(x=np.exp(x_range),
-                                y=np.exp(y_func(i)),
+        fig.add_trace(go.Scatter(x=lines.x,
+                                y=lines['quantile_0.5'],
                                 mode='lines',
                                 name='50% quantile (median)',
                                 line=dict(color='black', dash='dot', width= 2)))
     else:
         if i == 0.15:
-            fig.add_trace(go.Scatter(x=np.exp(x_range),
-                                    y=np.exp(quant(0.5, x_range)+boundary_gaps[letter]),
+            fig.add_trace(go.Scatter(x=lines.x,
+                                    y=lines['rating_'+letter],
                                     mode='lines',
                                     name='Rating Boundaries',
                                     opacity=0.4,
                                     line=dict(color='green', dash='dash', width= 2)))
-            fig.add_trace(go.Scatter(x=np.exp(x_range),
-                                    y=np.exp(y_func(i)),
+            fig.add_trace(go.Scatter(x=lines.x,
+                                    y=lines['quantile_'+str(i)],
                                     mode='lines',
                                     name='Quantile Regressions',
                                     line=dict(color='black', dash='solid', width= 2)))
         else:
-            fig.add_trace(go.Scatter(x=np.exp(x_range),
-                                    y=np.exp(quant(0.5, x_range)+boundary_gaps[letter]),
+            fig.add_trace(go.Scatter(x=lines.x,
+                                    y=lines['rating_'+letter],
                                     mode='lines',
                                     name='Rating Boundaries',
                                     showlegend=False,
                                     opacity=0.4,
                                     line=dict(color='green', dash='dash', width= 2)))
-            fig.add_trace(go.Scatter(x=np.exp(x_range),
-                                    y=np.exp(y_func(i)),
+            fig.add_trace(go.Scatter(x=lines.x,
+                                    y=lines['quantile_'+str(i)],
                                     mode='lines',
                                     name='Quantile Regressions',
                                     showlegend=False,
